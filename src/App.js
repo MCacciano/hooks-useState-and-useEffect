bfca6cf5b6670cd8f3c31a12313e1581854ca407
mcacciano <michael.cacciano@gmail.com>
2020-12-29 17:25:41 -0500

    useState and useEffect tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [allPokemonCards, setAllPokemonCards] = useState([]);
+  const [page, setPage] = useState(1);
+
+  // useEffect with an async call
+  useEffect(() => {
+    const asyncGetPokemon = async () => {
+      try {
+        const response = await fetch(
+          `https://api.pokemontcg.io/v1/cards?pageSize=20&page=${page}`
+        );
+        const data = await response.json();
+
+        setAllPokemonCards(data.cards);
+        setPage(prev => prev + 1);
+      } catch (err) {
+        console.error(err);
+        return () => null;
+      }
+    };
+
+    asyncGetPokemon();
+
+    return () => null;
+    // if dependency array is empty useEffect will only run on mount
+  }, []);
+
+  // show how the dependency array works
+  useEffect(() => {
+    if (allPokemonCards.length > 20) {
+      console.log('more than 20 cards');
+    }
+  }, [allPokemonCards]);
+
+  // getting more cards based on page num with useState
+  const handleGetMoreCards = async () => {
+    try {
+      const response = await fetch(
+        `https://api.pokemontcg.io/v1/cards?pageSize=20&page=${page}`
+      );
+      const data = await response.json();
+
+      setAllPokemonCards(prev => [...prev, ...data.cards]);
+      setPage(prev => prev + 1);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='header'>
+        <h1>Pokemon Cards</h1>
+        <button className='btn' type='button' onClick={handleGetMoreCards}>
+          Get More Cards
+        </button>
+      </div>
+      <ul className='pokemon-list'>
+        {allPokemonCards.map(card => {
+          return (
+            <li key={card.id} className='pokemon-list-item'>
+              <img src={card.imageUrl} />
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
